3981addee999aa9943bfd05f3da637d4636f1a5c
Duncan McLean <duncan.mclean@fiserv.com>
2023-11-02 13:13:03 -0400

    Completed AH7

--- Source.java
+++ Source.java
@@ -10,8 +10,27 @@ class Node<T> {
 
 class Source {
   public static int sumList(Node<Integer> head) {
-    // todo
-    return;
+    int sum = 0;
+
+    // If it is a single Node set ans to its value
+    if (head.next == null)
+      sum += head.val;
+
+    // Loops while there is another Node in the linked List
+    while (head.next != null) {
+
+      // Adds current value to the total value
+      sum += head.val;
+      head = head.next;
+
+      // Checks to see if it is the last Node and adds it to the total sum
+      if (head.next == null)
+        sum += head.val;
+
+    } // whiile
+
+    // Returns the sum of the linked list
+    return sum;
   }
 
   public static void main(String[] args) {
@@ -27,7 +46,7 @@ class Source {
     d.next = e;
 
     // 2 -> 8 -> 3 -> -1 -> 7
-    
+
     System.out.println("Sum of my list: ");
     System.out.println(Source.sumList(a));
   }
